c4c844ab5f547474d193f4f024936aaf27d3253b
Alexander <alexander.lique.l@uni.pe>
2021-04-24 10:20:17 -0500

    Se modificó el burbuja bidireccional

--- Algoritmos/Burbuja_bidireccional.cpp
+++ Algoritmos/Burbuja_bidireccional.cpp
@@ -8,6 +8,14 @@ void llenar(int [], int);
 void print(int [], int);
 void burbuja_bidireccional(int [100], int );
 
+/*
+<Documentación>
+
+Para iniciar el programa debe digitar el tamaño del arreglo, esto generará un
+arreglo aleatorio que posteriormente será ordenado e imprimiendo los pasos
+que realiza de derecha hacia izquierda y pasos de izquierda hacia derecha.
+
+*/
 int main(){
 
 /***********************************************************************************/
@@ -24,13 +32,16 @@ int main(){
     int array[dimension]; //Inicialiazamos el array
     llenar(array, dimension); //Llamamos a la función llenar.
     print(array, dimension);//LLamamos a la función print.
+
 /*************************************************************************************/   
+/*                    Aplicando la función de ordenación                               */
+
     burbuja_bidireccional(array, dimension); //Usamos la función burbuja bidireccional.
 
     return 0;
 }
 
-/************************ Creacion de Funciones *****************************************/
+/**************************** Creacion de Funciones *****************************************/
 
 void llenar(int array[], int size){
     /*
@@ -38,7 +49,7 @@ void llenar(int array[], int size){
         -Esta función se encargara de llenar de número aleatorios un arreglo.
     Argumentos:
         - arreglo: El primer parámetro es un arreglo de tipo (int).
-        - size: Este segundo parámetro de  tipo  (int) nos indicara el tamaño de este puntero.
+        - size: Este segundo parámetro de  tipo  (int) nos indicara el tamaño de este arreglo.
     */
     for(int i=0; i < size; i++){
         array[i] =  rand()%(100 + 1 -1) + 1;
@@ -51,7 +62,7 @@ void print(int  array [], int size){
         -Esta función se encargara de imprimir el puntero que le pasemos por parámetro.
     Argumentos:
         - arreglo: El primer parámetro es un puntero de tipo (int).
-        - size:    Este segundo parámetro de  tipo  (int) nos indicara el tamaño de este puntero.
+        - size:    Este segundo parámetro de  tipo  (int) nos indicara el tamaño de este arreglo.
     */
     for(int i=0; i < size; i++){
         cout <<array[i] <<" ";
@@ -60,13 +71,27 @@ void print(int  array [], int size){
 }
 
 
-void burbuja_bidireccional(int array[100], int size){
+void burbuja_bidireccional(int array[], int size){
+    /*
+        burbuja_bidireccional:
+            - Esta función se encargara de aplicar el ordenamiento burbuja bidireccional
+              Se usara un for para hacer  el recorrido de  der hacia la izq usando un
+              condicional de si el elemento anterior es mayor al posterior aplicamos un swap
+              y actualizando la posición ultimo para asi acotar.
+              de igual manera un segundo for para izq hacia la der y todo esto mientras 
+              izq < der.
+        Argumentos:
+            - array : El arreglo que usaremos para ordenar.
+            - size: El tamaño del arreglo.
+    */
 
 /*            Declaración e Inicialización de variable            */
     int i, j, izq = 1, der = size - 1, ultimo = size - 1, aux;
 
     do {
+
 /*            Paso hacia la izquierda           */
+
         for(i = der; i >= izq; i--){
             if(array[i-1] > array[i]){
                 aux = array[i];
@@ -75,11 +100,14 @@ void burbuja_bidireccional(int array[100], int size){
                 ultimo = i;
             }
         }
+
         izq = ultimo + 1;
+        cout<<"*********************************"<<endl;
         cout<<"Paso hacia la izquierda"<<endl;
         print(array, size);
 
 /*           Paso hacia la derecha              */
+
         for(j = izq; j <= der; j++){
             if(array[j-1] > array[j]){
                 aux = array[j];
@@ -88,9 +116,11 @@ void burbuja_bidireccional(int array[100], int size){
                 ultimo = j;
             }
         }
+        
         der = ultimo - 1;
         cout<<"Paso hacia la derecha"<<endl;
-        print(array, size);    
+        print(array, size);   
+        
     }while(izq < der);
 
 }
